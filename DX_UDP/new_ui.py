f63fec43f46dde9aafbec8f842542e8425dfd5ff
dingxiao88 <dingxiao88@163.com>
2021-01-11 16:20:52 +0800

    新增mqtt功能。

--- DX_UDP/new_ui.py
+++ DX_UDP/new_ui.py
@@ -11,7 +11,7 @@ import datetime
 
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtCore import Qt, QPropertyAnimation
-from PyQt5.QtGui import QMouseEvent, QCursor
+from PyQt5.QtGui import QMouseEvent, QCursor, QPixmap
 
 from ui_dx_new import *
 
@@ -88,6 +88,9 @@ class mainWin(QMainWindow, Ui_MainWindow):
         self.pushButton_DC_FYRun_Down.released.connect(lambda:DX_Control.DC_FY_XH_Dir_ControlReset(self))
         self.pushButton_DC_XHRun_Left.released.connect(lambda:DX_Control.DC_FY_XH_Dir_ControlReset(self))
         self.pushButton_DC_XHRun_Right.released.connect(lambda:DX_Control.DC_FY_XH_Dir_ControlReset(self))
+
+        pix = QPixmap('1.jpg')
+        self.label_mqttPic.setPixmap(pix)
 
         curr_time = datetime.datetime.now()
         time_str = datetime.datetime.strftime(curr_time,'%Y-%m-%d %H:%M:%S')
