29cbb073e41b61492e739765efc45f749777cd98
dingxiao88 <dingxiao88@163.com>
2021-02-18 16:29:28 +0800

    测试农历日历库。

--- DX_UDP/APP/LunarYear_test.py
+++ DX_UDP/APP/LunarYear_test.py
@@ -1,8 +1,12 @@
 
-from lunar_python import Lunar
+from lunar_python import Lunar, Solar
  
+# 通过指定年月日初始化阳历
+solar = Solar.fromYmd(1981, 10, 23)
+
 # 通过指定年月日初始化阴历
-lunar = Lunar.fromYmd(1981, 10, 23)
+# lunar = Lunar.fromYmd(1981, 11, 19)
+lunar = solar.getLunar()
  
 # 打印阴历
 print(lunar.toFullString())
